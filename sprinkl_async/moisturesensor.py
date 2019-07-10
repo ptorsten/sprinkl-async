c012431b47ddd10cabe242e41e794d0041014cf9
Patrik Torstensson <patrik.torstensson@gmail.com>
2019-07-10 06:18:01 +0000

    Remove unused imports

--- sprinkl_async/moisturesensor.py
+++ sprinkl_async/moisturesensor.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Awaitable, Callable, List
+from typing import Awaitable, Callable
 
 from .pageobject import PageObject
 
