88879c79452ab8838c569df9fba1297039d118f1
Patrik Torstensson <patrik.torstensson@gmail.com>
2019-07-14 07:37:26 +0000

    Fix broken lint

--- sprinkl_async/webhook_objects.py
+++ sprinkl_async/webhook_objects.py
@@ -14,13 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Awaitable, Callable
-
 from .dataobject import DictObject
 
 
 class WebhookBaseObject:
-    """Base object for all webhook objects"""
+    """Base object for all webhook objects."""
 
     def __init__(self, obj):
         """Intialize."""
